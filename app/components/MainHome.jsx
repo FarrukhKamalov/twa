d1dae4b9c501c7ccd2c9b1f296cf9ee8cc47e2fc
Ramziddin <mahmudovramziddin25@gmail.com>
2024-08-11 14:45:32 +0500

    gf

--- app/components/MainHome.jsx
+++ app/components/MainHome.jsx
@@ -14,7 +14,7 @@ const MainHome = () => {
 
               <div className="w-[250px] h-[]">
                 <img
-                  src="/assets/images/dog-game-removebg-preview.png"
+                  src="/assets/images/Artboard_2_2x-100-removebg-preview.png"
                   className="w-full"
                   alt="no image"
                 />
