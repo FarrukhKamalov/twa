a4a51310987c889025d28a220ad31443c166121c
Ramziddin <mahmudovramziddin25@gmail.com>
2024-08-11 22:47:45 +0500

    Dd

--- app/components/MainHome.jsx
+++ app/components/MainHome.jsx
@@ -9,7 +9,7 @@ const MainHome = () => {
         <div className=" container mx-auto">
           <div className="flex flex-col gap-5 items-center justify-center">
             <div className="flex flex-col items-center">
-              <button className="bg-white flex gap-2 items-center rounded-sm p-2 w-[120px] mt-4 mb-4">
+              <button className="bg-white flex gap-2 items-center rounded-sm p-2 w-[150px] mt-4 mb-4">
                 <MdCastConnected className="text-[11px]" />
                 <span className="text-[11px] font-medium">Connect wallet</span>
               </button>
