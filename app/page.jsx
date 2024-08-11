3729ba09ea58c8a184cae2d90983b7b2fd5d38e8
Ramziddin <mahmudovramziddin25@gmail.com>
2024-08-11 12:46:15 +0500

    Update Data

--- app/page.jsx
+++ app/page.jsx
@@ -19,14 +19,13 @@ function Home() {
     <main className="p-4">
       {userData ? (
         <>
-          <h1 className="text-2xl font-bold mb-4">User Data</h1>
+          <h1 className="text-2xl font-bold mb-4 text-white">User Data</h1>
           <ul>
-            <li>ID: {userData.id}</li>
-            <li>First Name: {userData.first_name}</li>
-            <li>Last Name: {userData.last_name || 'N/A'}</li>
-            <li>Username: {userData.username || 'N/A'}</li>
-            <li>Language Code: {userData.language_code}</li>
-            <li>Is Premium: {userData.is_premium ? 'Yes' : 'No'}</li>
+            <li className='text-white font-bold text-center'>ID: {userData.id}</li>
+            <span className='text-white font-bold text-center'>First Name: {userData.first_name}</span>
+            <span className='text-white font-bold text-center'>Last Name: {userData.last_name || 'N/A'}</span>
+            <li className='text-white font-bold text-center'>Username: {userData.username || 'N/A'}</li>
+            <li className='text-white font-bold text-center'>Is Premium: {userData.is_premium ? 'Yes' : 'No'}</li>
           </ul>
           <div>
             <MainHome />
