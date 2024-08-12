50e0e0b45afe73cc1a99bb6692bd9e5142db90b6
Ramziddin <mahmudovramziddin25@gmail.com>
2024-08-12 22:50:53 +0500

    Finished

--- app/components/Swiper.jsx
+++ app/components/Swiper.jsx
@@ -17,6 +17,7 @@ const Slider = () => {
       <Swiper
         spaceBetween={10}
         centeredSlides={true}
+        slidesPerView={1.2}
         autoplay={{
           delay: 2500,
           disableOnInteraction: true,
