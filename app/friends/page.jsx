0d79a09b659a656e9e4da790f31737ca94e5b9cd
Ramziddin <mahmudovramziddin25@gmail.com>
2024-08-17 08:30:37 +0500

    Finished Bro

--- app/friends/page.jsx
+++ app/friends/page.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 const page = () => {
+  
   return (
     <div className="w-full py-8">
       <div className="container mx-auto">
         <div className="">
           <div className="text-white text-[30px] items-center justify-center">
             <p className="text-center ">Invite friend <br /> and get more Blynd</p>
-            <img src="/assets/images/505.png" alt="jj" className="w-32 ml-24"/>
+            <img src="/assets/images/505.png" alt="jj" className="w-32 m-auto"/>
             <p className="text-start mt-14">0 friends</p>
             <button className="post">Invite Friend</button>
           </div>
--- app/leaderboard/page.jsx
+++ app/leaderboard/page.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
 import LeaderboardMain from "../components/LeaderboardMain";
 import LeadingbordHome from "../components/LeadingbordHome";
 
 const page = () => {
+  // const [posts, setPosts] = useState([]);
+
+  // useEffect(() => {
+  //   fetch('')
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setPosts(data.about);
+  //     })
+  // }, []);
+
+  // <div className="gdi">
+  //   {posts.map((user, id) => (
+  //     <div className="" key={id}>
+  //       <img src={user.img} className="grid-image" />
+  //       <p className="">{user.name}</p>
+  //       <p className="">{user.des}</p>
+  //     </div>
+  //   ))}
+  // </div>;
+
   return (
     <div>
       {/* <LeadingbordHome /> */}
